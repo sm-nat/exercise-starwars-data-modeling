1bb26366e946fe8a6817e86a7c4ff73436e51dbe
sm-nat <141369048+sm-nat@users.noreply.github.com>
2023-11-07 21:10:25 +0000

    tablas casi listas

--- src/models.py
+++ src/models.py
@@ -7,7 +7,7 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
+""" class Person(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
@@ -23,10 +23,58 @@ class Address(Base):
     street_number = Column(String(250))
     post_code = Column(String(250), nullable=False)
     person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    person = relationship(Person) """
 
-    def to_dict(self):
-        return {}
+
+class User(Base):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True)
+    user_name = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    name = Column(String, nullable=False)
+    last_name = Column(String, nullable=True)
+    email = Column(String, nullable=False)
+    
+
+class Character(Base):
+      __tablename__ = "character"
+      id = Column(Integer, primary_key=True)
+      char_name = Column(String, nullable=False)
+      planet_id = Column(Integer, ForeignKey('planet.id'), nullable=False)  # id del planeta de origen
+      planet = relationship("Planet")  # Relación con la tabla Planet
+
+
+class Planet(Base):
+     __tablename__ = "planet"
+     id = Column(Integer, primary_key=True)
+     planet_name = Column(String, nullable=False)
+
+class Weapon(Base):
+      __tablename__ = "weapon"
+      id = Column(Integer, primary_key=True)
+      weapon_name = Column(String, nullable=False)
+
+    
+class Favorites(Base):
+     __tablename__ = "favorites"
+     id = Column(Integer, primary_key=True)
+     user_id = Column(Integer, ForeignKey('user.id'), nullable=False)  # id del user
+     character_id = Column(Integer, ForeignKey('character.id'), nullable=False)  # id del characer
+     user = relationship("User")  # Relación con la tabla de user
+     character = relationship("Character")  # Relación con la tabla de personajes
+
+
+class Post(Base):
+     __tablename__ = "post"
+     id = Column(Integer, primary_key=True)
+     title = Column(String, nullable=False)  # Título de la publicación
+     content = Column(String, nullable=False)  # Contenido de la publicación
+     author_id = Column(Integer, ForeignKey('user.id'), nullable=False)  # id del autor de la publicación
+     author = relationship("User")  # Relación con la tabla de usuarios (autor de la publicación)
+
+
+def to_dict(self):
+    return {}
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
